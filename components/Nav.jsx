74e7b0c9c32a694dd7a42c64361486e2ab435505
Ajay Kumar <ajay.ce.kumar@avanade.com>
2023-07-30 20:19:22 +0200

    adding menu

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -21,34 +21,42 @@ function Nav() {
   if (!user) return null;
 
   return (
-    <div>
-      <aside id="sidebar-wrapper">
-        <div className="sidebar-brand">
-          <h2>
-            <i className="fa fa-user"></i>
-            Ror Vansaj
-          </h2>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          Ror
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" href="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/users">
+                Users
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#" onClick={userService.logout}>
+                Log out
+              </Link>
+            </li>
+          </ul>
         </div>
-
-        <ul className="sidebar-nav">
-          <li className="nav-item">
-            <Link href="/">
-              <i className="fa-fw fas fa-home nav-icon"></i>&nbsp; Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link href="/users">
-              <i className="fa-fw fas fa-list nav-icon"></i>&nbsp; Users
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link onClick={userService.logout} href="#">
-              <i className="nav-icon fas fa-fw fa-sign-out-alt"></i>&nbsp;
-              Logout
-            </Link>
-          </li>
-        </ul>
-      </aside>
-    </div>
+      </div>
+    </nav>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -7,13 +7,15 @@ function Document() {
     <Html lang="en">
       <Head>
         <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
         />
         <link
           href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
           rel="stylesheet"
         />
+        <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
       </Head>
 
       <body>
