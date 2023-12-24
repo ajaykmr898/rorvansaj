10625abfd1b58faaff2ed0ab67eadeb16af31599
Ajay Kumar <ajay.ce.kumar@avanade.com>
2023-12-24 15:46:18 +0100

    fixing gotra page

--- pages/gotras/index.jsx
+++ pages/gotras/index.jsx
@@ -10,124 +10,76 @@ import AddIcon from "@mui/icons-material/AddOutlined";
 import InfoIcon from "@mui/icons-material/InfoOutlined";
 import { Button } from "@mui/material";
 
-import { useRef } from "react";
-import Chart from "chart.js";
+import { ResponsiveLine } from "@nivo/line";
 
 const RangeAreaChart = () => {
-  const dataRef = useRef([]);
-  const [data, setData] = useState([]);
-
-  const handleChange = (newData) => {
-    const boundHandleChange = handleChange.bind(this);
-    boundHandleChange(newData);
-    renderChart();
-  };
-
-  const renderChart = () => {
-    const ctx = document.getElementById("myChart").getContext("2d");
-    const chart = new Chart(ctx, {
-      type: "line",
-      data: {
-        datasets: [
-          {
-            label: "Range Area",
-            data: dataRef.current.map((date) => [
-              date.x,
-              date.low1,
-              date.high1,
-            ]),
-            fill: false,
-            borderColor: "#ffff00",
-            borderWidth: 2,
-          },
-        ],
-        xAxis: {
-          type: "date",
-          categories: dataRef.current.map((date) => date.x),
-        },
-        yAxes: [
-          { id: "low1", title: "Low 1", min: 0 },
-          { id: "high1", title: "High 1", min: 0 },
-        ],
-      },
-      options: {
-        responsive: true,
-        scales: {
-          xAxes: [
-            {
-              type: "time",
-              tooltip: true,
-              time: { parser: "iso8601" },
-            },
-          ],
-          yAxes: [
-            { id: "low1", ticks: { beginAtZero: true } },
-            { id: "high1", ticks: { beginAtZero: true } },
-          ],
-        },
-      },
-      plugins: ["afterDatasetsDraw"],
-    });
-
-    chart.options.afterDatasetsDraw = (chart) => {
-      const ctx = chart.ctx;
-      const rangeArea = Object.assign(chart.data.datasets[0], {
-        type: "area",
-        borderWidth: 0,
-        fill: "#ffff00",
-      });
-
-      const rangePoints = [];
-      dataRef.current.forEach((date) => {
-        const range = date.high1 - date.low1;
-        rangePoints.push({ x: date.x, y: date.low1 + range / 2 });
-      });
-
-      const rangeAreaPath = ctx.createArea();
-      rangeAreaPath.moveTo(rangePoints[0].x, rangePoints[0].y);
-      for (let i = 1; i < rangePoints.length; i++) {
-        const dataPoint = rangePoints[i];
-        rangeAreaPath.lineTo(dataPoint.x, dataPoint.y);
-      }
-      rangeAreaPath.fill();
-      ctx.fillStyle = "#ffff00";
-      ctx.fill();
-    };
-  };
+  // Data for the range area chart
+  const data = [
+    {
+      id: "Range1",
+      data: [
+        { x: "2023-01-01", y: 5 },
+        { x: "2023-01-02", y: 10 },
+        { x: "2023-01-03", y: 15 },
+        // Add more data as needed
+      ],
+    },
+    {
+      id: "Range2",
+      data: [
+        { x: "2023-01-01", y: 8 },
+        { x: "2023-01-02", y: 12 },
+        { x: "2023-01-03", y: 17 },
+        // Add more data as needed
+      ],
+    },
+  ];
 
   return (
-    <div>
-      <canvas id="myChart"></canvas>
-      <button
-        onClick={(event) => {
-          event.preventDefault();
-          handleChange([
-            {
-              x: new Date("2023-01-01"),
-              low1: 100,
-              high1: 150,
-              low2: 80,
-              high2: 120,
-            },
-            {
-              x: new Date("2023-01-02"),
-              low1: 110,
-              high1: 160,
-              low2: 90,
-              high2: 130,
-            },
-            {
-              x: new Date("2023-01-03"),
-              low1: 120,
-              high1: 170,
-              low2: 100,
-              high2: 140,
-            },
-          ]);
+    <div style={{ height: "400px" }}>
+      <ResponsiveLine
+        data={data}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: "point" }}
+        yScale={{
+          type: "linear",
+          min: "auto",
+          max: "auto",
+          stacked: true,
+          reverse: false,
         }}
-      >
-        Change Data
-      </button>
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          orient: "bottom",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Date",
+          legendOffset: 36,
+          legendPosition: "middle",
+        }}
+        axisLeft={{
+          orient: "left",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Y-axis",
+          legendOffset: -40,
+          legendPosition: "middle",
+        }}
+        colors={{ scheme: "category10" }}
+        enablePoints={false}
+        enableGridX={false}
+        enableGridY={false}
+        enableArea={true}
+        areaBaselineValue={0}
+        areaOpacity={0.3}
+        enableSlices="x"
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+      />
     </div>
   );
 };
